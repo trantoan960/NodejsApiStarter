a01868b6622faf3f610624dd6314d559debb74a0
Tran Toan <trantoan@Trans-MacBook-Pro.local>
2020-04-09 02:38:25 +0700

    🔥 Commit finnish video #02.

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const logger = require('morgan')
+
+const app = express()
+
+// Middlewares
+app.use(logger('dev'))
+
+// Routes
+app.get('/', (req, res, next) => {
+    return res.status(200).json({
+        message: 'Server is OK!'
+    })
+})
+
+// Catch 404 Errors and forward them to error handler
+app.use((req, res, next) => {
+    const err = new Error('Not Found')
+    err.status = 404
+    next(err)
+})
+
+// Error handler function
+app.use(() => {
+    const error = app.get('env') === 'development' ? err : {}
+    const status = err.status || 500
+
+    // response to client
+    return res.status(status).json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
+// Start the server
+const port = app.get('port') || 3000
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
